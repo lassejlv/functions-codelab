d30142a8dbb37e377520828c78f6466fc0640519
lassev05 <vestergaardlasse2@gmail.com>
2023-11-15 09:37:48 +0100

    Second Commit

--- assets/js/functionsThree.js
+++ assets/js/functionsThree.js
@@ -1,17 +1,15 @@
-
-
-
 // her skal du skrive din anonyme funktion
-let myAnonFunc=
-
+let myAnonFunc = function () {
+  return "Hello World";
+};
 
 // her kaldes, eller invokes din anonyme funktion gennem en variabel
 console.log(myAnonFunc());
 
-
 // her skal du skrive din arrow funktion
-let myArrowFunc=
-
+let myArrowFunc = () => {
+  return "Hello World";
+};
 
 // her kaldes, eller invokes din arrow funktion gennem en variabel
 console.log(myArrowFunc());
--- assets/js/valutaexchangeThree.js
+++ assets/js/valutaexchangeThree.js
@@ -1,43 +1,41 @@
-
 //Dom elementer jeg har brug for
 // knap til udregning
-const calculateButton = document.getElementById('calculateButt');
+const calculateButton = document.getElementById("calculateButt");
 
-
-calculateButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    //alert('Jeg vil gerne kalde en funktion der kan regne valutaen ud...');
-    gatherInputData();
+calculateButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  //alert('Jeg vil gerne kalde en funktion der kan regne valutaen ud...');
+  gatherInputData();
 });
 
-
-
-
 // indsamling af data fra dom (model code fra MVC)
+function gatherInputData() {
+  const myEuroRate = document.getElementById("euroRate").value;
+  const myDollarRate = document.getElementById("dollarRate").value;
 
+  const myDanishAmount = document.getElementById("danishAmount").value;
 
-
-
+  makeExchange(myEuroRate, myDollarRate, myDanishAmount);
+}
 
 // håndterer data behandling  (controller code fra MVC)
+function makeExchange(myEuroRate, myDollarRate, myDanishAmount) {
+  let myDollars = calculateExchange(myDanishAmount, myDollarRate);
+  let myEuros = calculateExchange(myDanishAmount, myEuroRate);
 
-
+  showResult(myEuros, myDollars, myDanishAmount);
+}
 
 // sub funktion der udfører en opgave og returnerer et resultat.  stadig controller code i MVC
 
-
-
-
 // viser resultatet  (view code fra MVC)
 function showResult(myEuroResult, myDollarResult, myDanishAmount) {
-    const resultElement = document.getElementById('result');
-    resultElement.innerHTML = myDanishAmount + ' danske kroner bliver til: ' + myEuroResult + ' euro og ' + myDollarResult + ' dollars';
-
+  const resultElement = document.getElementById("result");
+  resultElement.innerHTML =
+    myDanishAmount +
+    " danske kroner bliver til: " +
+    myEuroResult +
+    " euro og " +
+    myDollarResult +
+    " dollars";
 }
-
-
-
-
-
-
-
